f85d26f074f5b0563640f1f9cc3a0a46ab7eb54d
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2025-05-31 19:02:41 +0200

    [MISC] move zig build into project folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import zigar from 'rollup-plugin-zigar';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import zigar from "rollup-plugin-zigar";
+import path from "path";
+
+const ZIGAR_BUILD = "zigar-build";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), zigar()],
-})
+  plugins: [
+    vue(),
+    zigar({
+      buildDir: path.join(__dirname, ZIGAR_BUILD),
+    }),
+  ],
+});
